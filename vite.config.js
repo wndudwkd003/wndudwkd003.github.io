9efa01daf0195a2df76549df135c03796543b77f
wndudwkd003 <ymail3@naver.com>
2025-12-10 12:14:48 +0900

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "/", // 또는 base 항목 자체가 없어도 기본이 '/'라서 괜찮습니다.
+});
