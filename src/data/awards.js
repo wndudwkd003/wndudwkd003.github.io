8dce6ddb9ee9d560231b618b5c561520ae6ddeba
wndudwkd003 <ymail3@naver.com>
2025-12-20 12:05:34 +0900

    mobisec2025

--- src/data/awards.js
+++ src/data/awards.js
@@ -1,189 +1,191 @@
 // src/data/awards.js
 
 export const awards = [
-  {
-    id: "f1tenth-kc4_2025-11-6_1",
-    title: "The 4th F1tenth Korea Championship 2025 장려상 (5th Place)",
-    org: "F1tenth Korea",
-    date: "2025-11-06",
-    description: "국제 자율주행 플랫폼 경주 대회",
-    tags: ["자율주행", "경진대회"],
+    {
+        id: "mobisec2025_2025-12-17",
+        title: "The 9th International Conference on Mobile Internet Security (MobiSec 2025)",
+        org: "MobiSec 2025",
+        date: "2025-12-17",
+        description: "MobiSec 국제 학술대회 논문 발표",
+        tags: ["학술대회"],
 
-    images: { count: 4 },
-    downloads: [],
-  },
+        images: { count: 7 },
+        downloads: [],
+    },
 
-  {
-    id: "ictc2025_2025-10-15_1",
-    title: "The 16th International Conference on ICT Convergence (ICTC 2025)",
-    org: "IEEE ICTC 2025",
-    date: "2025-11-06",
-    description: "ICTC 국제 학술대회 논문 발표",
-    tags: ["학술대회"],
+    {
+        id: "f1tenth-kc4_2025-11-6_1",
+        title: "The 4th F1tenth Korea Championship 2025 장려상 (5th Place)",
+        org: "F1tenth Korea",
+        date: "2025-11-06",
+        description: "국제 자율주행 플랫폼 경주 대회",
+        tags: ["자율주행", "경진대회"],
 
-    images: { count: 4 },
-    downloads: [],
-  },
-  {
-    id: "hclt-kacl_2025-10-01_1",
-    title: "국립국어원 인공지능 말평 활용 연구 최우수·우수 공동수상",
-    org: "국립국어원",
-    date: "2025-10-01",
-    description: "2025년도 HCLT-KACL 공동 학술대회 국립국어원 인공지능 말평 활용 연구",
-    tags: ["학술대회", "자연어처리", "문화체육관광부장관상", "국립국어원장상"],
+        images: { count: 4 },
+        downloads: [],
+    },
 
-    images: { count: 7 },
-    downloads: [
-      { type: "pdf", file: "상장_최우수상.pdf", down: true },
-      { type: "pdf", file: "상장_우수상.pdf", down: true },
-    ],
-  },
+    {
+        id: "ictc2025_2025-10-15_1",
+        title: "The 16th International Conference on ICT Convergence (ICTC 2025)",
+        org: "IEEE ICTC 2025",
+        date: "2025-11-06",
+        description: "ICTC 국제 학술대회 논문 발표",
+        tags: ["학술대회"],
 
-  {
-    id: "융복합ICT활용공모전_2025-01-24",
-    title: "융복합 ICT 활용 공모전 우수상",
-    org: "경상국립대학교 정보전산처",
-    date: "2025-01-10",
-    description: "2024학년도 융복합 ICT 활용 공모전 우수상 수상",
-    tags: ["공모전"],
+        images: { count: 4 },
+        downloads: [],
+    },
+    {
+        id: "hclt-kacl_2025-10-01_1",
+        title: "국립국어원 인공지능 말평 활용 연구 최우수·우수 공동수상",
+        org: "국립국어원",
+        date: "2025-10-01",
+        description: "2025년도 HCLT-KACL 공동 학술대회 국립국어원 인공지능 말평 활용 연구",
+        tags: ["학술대회", "자연어처리", "문화체육관광부장관상", "국립국어원장상"],
 
-    images: { count: 5 },
-    downloads: [
-      { type: "pdf", file: "상장_우수상.pdf", down: true },
-    ],
-  },
+        images: { count: 7 },
+        downloads: [
+            { type: "pdf", file: "상장_최우수상.pdf", down: true },
+            { type: "pdf", file: "상장_우수상.pdf", down: true },
+        ],
+    },
 
-  {
-    id: "f1tenth-psu_2025-01-23",
-    title: "F1Tenth PNU-HYUNSONG Championship 2025 (장려상)",
-    org: "부산대학교",
-    date: "2025-01-23",
-    description: "F1Tenth PNU-HYUNSONG Championship 2025 장려상 수상",
-    tags: ["자율주행", "경진대회"],
+    {
+        id: "융복합ICT활용공모전_2025-01-24",
+        title: "융복합 ICT 활용 공모전 우수상",
+        org: "경상국립대학교 정보전산처",
+        date: "2025-01-10",
+        description: "2024학년도 융복합 ICT 활용 공모전 우수상 수상",
+        tags: ["공모전"],
 
-    images: { count: 10 },
-    downloads: [
-      { type: "pdf", file: "상장_장려상.pdf", down: true },
-    ],
-  },
+        images: { count: 5 },
+        downloads: [{ type: "pdf", file: "상장_우수상.pdf", down: true }],
+    },
 
-  {
-    id: "usg-제조경진대회_2025-01-16",
-    title: "USG AI 데이터 문제해결 제조혁신 경진대회 장려상",
-    org: "USG 스마트제조ICT사업단",
-    date: "2025-01-10",
-    description: "제5차 USG AI 데이터 문제해결 제조혁신 경진대회 장려상 수상",
-    tags: ["USG", "경진대회"],
+    {
+        id: "f1tenth-psu_2025-01-23",
+        title: "F1Tenth PNU-HYUNSONG Championship 2025 (장려상)",
+        org: "부산대학교",
+        date: "2025-01-23",
+        description: "F1Tenth PNU-HYUNSONG Championship 2025 장려상 수상",
+        tags: ["자율주행", "경진대회"],
 
-    images: { count: 5 },
-    downloads: [
-      { type: "pdf", file: "상장_장려상.pdf", down: true },
-    ],
-  },
+        images: { count: 10 },
+        downloads: [{ type: "pdf", file: "상장_장려상.pdf", down: true }],
+    },
 
-  {
-    id: "창의융합동아리_2025-01-10",
-    title: "경상국립대학교 GNU창의융합동아리 활동 보고대회 우수상",
-    org: "경상국립대학교",
-    date: "2025-01-10",
-    description: "2024학년도 GNU창의융합동아리 활동 보고대회 우수상",
-    tags: ["교내대회", "창의융합동아리"],
+    {
+        id: "usg-제조경진대회_2025-01-16",
+        title: "USG AI 데이터 문제해결 제조혁신 경진대회 장려상",
+        org: "USG 스마트제조ICT사업단",
+        date: "2025-01-10",
+        description: "제5차 USG AI 데이터 문제해결 제조혁신 경진대회 장려상 수상",
+        tags: ["USG", "경진대회"],
 
-    images: { count: 4 },
-    downloads: [
-    ],
-  },
+        images: { count: 5 },
+        downloads: [{ type: "pdf", file: "상장_장려상.pdf", down: true }],
+    },
 
-  {
-    id: "캡스톤디자인-학과_2024-12-09",
-    title: "2024학년도 연구성과 경진대회 및 제33회 소프트웨어 전시회 우수상·장려상 공동수상",
-    org: "경상국립대학교 컴퓨터공학과",
-    date: "2024-12-09",
-    description: "경상국립대학교 IT공과대학 컴퓨터공학과 우수상, 장려상 공동 수상",
-    tags: ["캡스톤디자인"],
+    {
+        id: "창의융합동아리_2025-01-10",
+        title: "경상국립대학교 GNU창의융합동아리 활동 보고대회 우수상",
+        org: "경상국립대학교",
+        date: "2025-01-10",
+        description: "2024학년도 GNU창의융합동아리 활동 보고대회 우수상",
+        tags: ["교내대회", "창의융합동아리"],
 
-    images: { count: 2 },
-    downloads: [
-      { type: "pdf", file: "보조개_우수상.pdf", down: true },
-      { type: "pdf", file: "제발공부하자_장려상.pdf", down: true },],
-  },
+        images: { count: 4 },
+        downloads: [],
+    },
 
-  {
-    id: "mobiesec2024_2024-12-18",
-    title: "The 8th International Conference on Mobile Internet Security (MobiSec 2024)",
-    org: "IEEE MobiSec 2024",
-    date: "2024-12-18",
-    description: "MobiSec 국제 학술대회 논문 발표",
-    tags: ["학술대회"],
+    {
+        id: "캡스톤디자인-학과_2024-12-09",
+        title: "2024학년도 연구성과 경진대회 및 제33회 소프트웨어 전시회 우수상·장려상 공동수상",
+        org: "경상국립대학교 컴퓨터공학과",
+        date: "2024-12-09",
+        description: "경상국립대학교 IT공과대학 컴퓨터공학과 우수상, 장려상 공동 수상",
+        tags: ["캡스톤디자인"],
 
-    images: { count: 6 },
-    downloads: [],
-  },
+        images: { count: 2 },
+        downloads: [
+            { type: "pdf", file: "보조개_우수상.pdf", down: true },
+            { type: "pdf", file: "제발공부하자_장려상.pdf", down: true },
+        ],
+    },
 
-  {
-    id: "k-health_2024-11-20_1",
-    title: "K-Health 의료 인공지능 해커톤 대회 최우수상",
-    org: "한국스마트헬스케어협회",
-    date: "2024-11-20",
-    description: "맘모그래피 영상데이터 활용 유방종괴 영역 분할 AI 학습 모델 개발",
-    tags: ["해커톤", "컴퓨터비전"],
+    {
+        id: "mobiesec2024_2024-12-18",
+        title: "The 8th International Conference on Mobile Internet Security (MobiSec 2024)",
+        org: "MobiSec 2024",
+        date: "2024-12-18",
+        description: "MobiSec 국제 학술대회 논문 발표",
+        tags: ["학술대회"],
 
-    images: { count: 4 },
-    downloads: [
-      { type: "pdf", file: "상장파일.pdf", down: true },
-    ],
-  },
+        images: { count: 6 },
+        downloads: [],
+    },
 
-  {
-    id: "f1tenth-kc3-2024-10-31",
-    title: "The 3th F1tenth Korea Championship 2025 (The 5th Prize)",
-    org: "F1tenth Korea",
-    date: "2024-10-31",
-    description: "국제 자율주행 플랫폼 경주 대회",
-    tags: ["자율주행", "경진대회"],
+    {
+        id: "k-health_2024-11-20_1",
+        title: "K-Health 의료 인공지능 해커톤 대회 최우수상",
+        org: "한국스마트헬스케어협회",
+        date: "2024-11-20",
+        description: "맘모그래피 영상데이터 활용 유방종괴 영역 분할 AI 학습 모델 개발",
+        tags: ["해커톤", "컴퓨터비전"],
 
-    images: { count: 7 },
-    downloads: [],
-  },
+        images: { count: 4 },
+        downloads: [{ type: "pdf", file: "상장파일.pdf", down: true }],
+    },
 
+    {
+        id: "f1tenth-kc3-2024-10-31",
+        title: "The 3th F1tenth Korea Championship 2025 (The 5th Prize)",
+        org: "F1tenth Korea",
+        date: "2024-10-31",
+        description: "국제 자율주행 플랫폼 경주 대회",
+        tags: ["자율주행", "경진대회"],
 
-  {
-    id: "f1tenth-iv2024_2024-06-05",
-    title: "F1 Tenth Autonomous Racing Competition IV 2024 Race Winner 1st Place",
-    org: "F1Tenth, The IEEE Intelligent Vehicles Symposium (IV) Jeju Korea",
-    date: "2024-06-05",
-    description: "국제 자율주행 경주 대회 우승",
-    tags: ["자율주행", "경진대회"],
+        images: { count: 7 },
+        downloads: [],
+    },
 
-    images: { count: 6 },
-    downloads: [
-      { type: "pdf", file: "race_winner.pdf", down: true },
-    ],
-  },
+    {
+        id: "f1tenth-iv2024_2024-06-05",
+        title: "F1 Tenth Autonomous Racing Competition IV 2024 Race Winner 1st Place",
+        org: "F1Tenth, The IEEE Intelligent Vehicles Symposium (IV) Jeju Korea",
+        date: "2024-06-05",
+        description: "국제 자율주행 경주 대회 우승",
+        tags: ["자율주행", "경진대회"],
 
-  {
-    id: "f1tenth-icra2024_2024-05-14",
-    title: "F1 Tenth Autonomous Racing Competition ICRA 2024",
-    org: "F1Tenth, IEEE Robotics & Automation Society (ICRA 2024) YOKOHAMA Japan",
-    date: "2024-05-14",
-    description: "국제 자율주행 경주 대회 참가",
-    tags: ["자율주행", "경진대회"],
+        images: { count: 6 },
+        downloads: [{ type: "pdf", file: "race_winner.pdf", down: true }],
+    },
 
-    images: { count: 8 },
-    downloads: [],
-  },
+    {
+        id: "f1tenth-icra2024_2024-05-14",
+        title: "F1 Tenth Autonomous Racing Competition ICRA 2024",
+        org: "F1Tenth, IEEE Robotics & Automation Society (ICRA 2024) YOKOHAMA Japan",
+        date: "2024-05-14",
+        description: "국제 자율주행 경주 대회 참가",
+        tags: ["자율주행", "경진대회"],
 
-  {
-    id: "캡스톤디자인-공학교육혁신센터_2024-02-08",
-    title: "2023년 GNU 사업성과 공유확산 및 경진대회 공동 우수상",
-    org: "경상국립대학교 공학교육혁신센터",
-    date: "2024-05-14",
-    description: "경상국립대학교 공학교육혁신센터 캡스톤디자인 경진대회 소실험실 공동 우수상 수상",
-    tags: ["소실험실", "캡스톤디자인"],
+        images: { count: 8 },
+        downloads: [],
+    },
 
-    images: { count: 2 },
-    downloads: [
-      { type: "pdf", file: "상장_소실험실_우수상.pdf", down: true },
-      { type: "pdf", file: "상장_교과_우수상.pdf", down: true },],
-  },
+    {
+        id: "캡스톤디자인-공학교육혁신센터_2024-02-08",
+        title: "2023년 GNU 사업성과 공유확산 및 경진대회 공동 우수상",
+        org: "경상국립대학교 공학교육혁신센터",
+        date: "2024-05-14",
+        description: "경상국립대학교 공학교육혁신센터 캡스톤디자인 경진대회 소실험실 공동 우수상 수상",
+        tags: ["소실험실", "캡스톤디자인"],
+
+        images: { count: 2 },
+        downloads: [
+            { type: "pdf", file: "상장_소실험실_우수상.pdf", down: true },
+            { type: "pdf", file: "상장_교과_우수상.pdf", down: true },
+        ],
+    },
 ];
